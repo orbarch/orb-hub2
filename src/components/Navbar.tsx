4adc4e8fc0b7d470439ec287b02839a76f8fee0e
duchess <duchessmein@gmail.com>
2025-11-04 17:15:01 -0700

    moved university to its own page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,6 +21,12 @@ export default function Navbar() {
                 Home
               </Link>
               <Link
+                href="/orbuniversity"
+                className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+              >
+                ORB University
+              </Link>
+              <Link
                 href="/categories"
                 className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
               >
